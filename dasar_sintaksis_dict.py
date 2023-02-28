27bc0c08400f9e232599c3170ac0f668c40a5f9c
Qaulan Tsaqila <qaulantsaqila75@gmail.com>
2023-02-28 19:01:20 +0700

    dictionary

--- dasar_sintaksis_dict.py
+++ dasar_sintaksis_dict.py
@@ -1,10 +1,35 @@
 users = {
-    'id' : 1,
-    'name': 'Leanne Graham',
-    'username': 'Bret',
-    'email': 'Sincere@april.biz',
+    "id" : 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street":"Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
 }
 print(users)
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"])
+print(users["address"]["street"])
+print(users["address"]["geo"]["lng"])
+
+print(users)
+print(type(users))
+#mengubah python dictionary menjadi json
+print("\nUbah dictionary ke json")
+import json
+result = json.dumps(users)
+print(type(result))
+print(result)
+
+with open('result.json','w') as file:
+    json.dump(users, file)
